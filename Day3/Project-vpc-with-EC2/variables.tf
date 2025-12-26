e75073bae7035972dc45b6eb9910b49d60f1d4d4
iam-shetty <kaveeshshetty6@gmail.com>
2025-12-26 07:35:14 +0000

    Project-vpc

---
+++ Day3/Project-vpc-with-EC2/variables.tf
@@ -0,0 +1,15 @@
+variable "cidr" {
+    type = string
+    description = "The Project VPC CIDR Block"
+}
+
+variable "ami" {
+    type = string
+    description = "The Project Region"
+  
+}
+variable "instance_type" {
+    type = string
+    description = "The Project Instance Type"
+  
+}
