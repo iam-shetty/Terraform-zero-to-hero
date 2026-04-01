a0ce903037bf925fb0649ce12b11c57d7542aef5
iam-shetty <kaveeshshetty6@gmail.com>
2026-04-01 17:08:30 +0000

    Terraform Functions

---
+++ Day10-11/outputs.tf
@@ -0,0 +1,10 @@
+output "project_name" {
+    description = "The name of the project."
+    value       = local.project_name
+  
+}
+output "bucket_name" {
+    description = "The name of the S3 bucket."
+    value       = local.bucket_name
+  
+}
